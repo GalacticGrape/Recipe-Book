ab8c6b3f0d9041e8f74af788b0b35e25855b7137
GalacticGrape <john.m.pung@gmail.com>
2023-10-18 15:44:33 -0700

    removed duplicated files

--- Python/Recipe Book/DB-Scripts/db_connection.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-print("Starting the script...")
-def get_db_connection ():
-    return sqlite3.connect(r"S:\Python\Recipe Book\Data\recipe_database.db")
-connection = get_db_connection()
-print("Database connection:", connection) 
