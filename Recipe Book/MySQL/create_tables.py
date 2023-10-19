489a8ca55d84a07f82989ec950be86fb5d7bc8ea
GalacticGrape <john.m.pung@gmail.com>
2023-10-18 17:08:06 -0700

    Update to create_tables.py for error handling

--- Recipe Book/MySQL/create_tables.py
+++ Recipe Book/MySQL/create_tables.py
@@ -14,36 +14,46 @@ db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 db_name = os.getenv("DB_NAME")
 
-# Create a connection to the database
-connection = mysql.connector.connect(
-    host=db_host,
-    user=db_user,
-    password=db_password,
-    database=db_name
-)
+try:
+    # Create a connection to the database
+    connection = mysql.connector.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_name
+    )
 
-# Create a cursor object using the connection
-cursor = connection.cursor()
+    # Create a cursor object using the connection
+    cursor = connection.cursor()
 
-# Now you can execute SQL queries using the cursor
-# For example, creating tables
-create_table_query = """
-CREATE TABLE IF NOT EXISTS recipe_categories (
-    category_name VARCHAR(255) PRIMARY KEY
-);
+    # Now you can execute SQL queries using the cursor
+    # For example, creating tables
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS recipe_categories (
+        category_name VARCHAR(255) PRIMARY KEY
+    );
 
-CREATE TABLE IF NOT EXISTS recipes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    ingredients TEXT,
-    instructions TEXT,
-    category_name VARCHAR(255),
-    FOREIGN KEY (category_name) REFERENCES recipe_categories(category_name)
-);
-"""
+    CREATE TABLE IF NOT EXISTS recipes (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255),
+        ingredients TEXT,
+        instructions TEXT,
+        category_name VARCHAR(255),
+        FOREIGN KEY (category_name) REFERENCES recipe_categories(category_name)
+    );
+    """
 
-cursor.execute(create_table_query)
+    cursor.execute(create_table_query)
 
-# Commit the changes and close the connection
-connection.commit()
-connection.close()
+    # Commit the changes
+    connection.commit()
+
+except mysql.connector.Error as error:
+    print("Error:", error)
+
+finally:
+    # Close the connection in the 'finally' block to ensure it's always closed
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection closed.")
