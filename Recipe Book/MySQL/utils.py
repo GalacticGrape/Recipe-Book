fc134337a9ba35340bcafc8143573e082becd663
GalacticGrape <john.m.pung@gmail.com>
2023-11-28 13:24:01 -0800

    updates to utils.py and main.py

--- Recipe Book/MySQL/utils.py
+++ Recipe Book/MySQL/utils.py
@@ -72,12 +72,73 @@ def get_categories(connection: MySQLConnection) -> List[str]:
         cursor.close()
     return categories
 
+def insert_recipe(recipe_name, instructions, category_name, connection):
+    # global db_host, db_user, db_password, db_name  
+        # Declare variables as global
+        # commented out to troubleshoot
+
+    """
+    Insert a recipe into the database.
+
+    Args:
+    mysql (module): The mysql module for database connection.
+    recipe_name (str): The name of the recipe.
+    instructions (str): Instructions for the recipe.
+    category_name (str): The category of the recipe.
+
+    Returns:
+    bool: True if insertion is successful, False otherwise.
+    """
+    # In this section, you define the insert_recipe function
+    # This function takes three arguments: recipe_name, instructions, and category_name
+    # Inside this function, you will write the code to insert these values into the database
+
+    try:               
+        # Create a connection to the database
+        # Create a cursor object using the connection
+        with connection.cursor() as cursor:
+            # Insert query
+            insert_recipe_query = """
+                INSERT INTO recipes (name, instructions, category_name)
+                VALUES (%s, %s, %s)
+                """
+
+            # Data for the recipe
+            recipe_data = (recipe_name, instructions, category_name)
+
+            # Execute the insert query with the recipe data
+            cursor.execute(insert_recipe_query, recipe_data)
+
+            # Commit the changes to the database
+            connection.commit()
+
+        return True
+        # Inside the try block, a connection to the database is established using the retrieved environment variables (db_host, db_user, db_password, db_name)
+        # The recipe details provided as arguments are inserted into the recipes table using an SQL INSERT INTO query
+        # The commit method is called to make the changes permanent in the database. If the insertion is successful, the function returns True
+
+
+    except mysql.connector.Error as error:
+        print("Error:", error)
+        return False
+        # In case there is an error during the database operations, the code inside the except block will execute
+        # It prints the error message and returns False to indicate that the insertion failed
+
+
+    finally:
+        # Close the connection in the 'finally' block to ensure it's always closed
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection closed.")
+        # The finally block ensures that the database connection is closed properly, regardless of whether the operations were successful or not
+        # It checks if the connection is still open, and if so, it closes both the cursor and the connection
 
 def insert_recipe_flow(connection_obj):
     # Get recipe details from user input
     recipe_name = input("Enter recipe name: ")
     instructions = input("Enter recipe instructions: ")  
-    categories = get_categories()
+    categories = get_categories(connection_obj)
     
     # Prompt user for category choice
     print("Available categories:")
@@ -88,8 +149,13 @@ def insert_recipe_flow(connection_obj):
     category_choice = int(input("Choose a category: ")) - 1
     selected_category = categories[category_choice]
     
+    # Print values for troubleshooting
+    print(f"Recipe Name: {recipe_name}")
+    print(f"Instructions: {instructions}")
+    print(f"Selected Category: {selected_category}")
+    
     # Insert the recipe into the database
-    if insert_recipe(recipe_name, instructions, selected_category, connection):
+    if insert_recipe(recipe_name, instructions, selected_category, connection_obj):
         print("Recipe inserted successfully!")
     else:
         print("Failed to insert recipe.")
